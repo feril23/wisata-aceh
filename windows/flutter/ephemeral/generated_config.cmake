1edd9cb73bd99037f87407bfc88c2c24fdaac0f7
feril23 <41193893+feril23@users.noreply.github.com>
2024-12-23 15:47:24 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Kuliah\\TI\\Semester 5\\Konsep Aplikasi dan Web\\Aplikasi\\flutter_application_2" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\src\\flutter"
+  "PROJECT_DIR=D:\\Kuliah\\TI\\Semester 5\\Konsep Aplikasi dan Web\\Aplikasi\\flutter_application_2"
+  "FLUTTER_ROOT=C:\\flutter\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Kuliah\\TI\\Semester 5\\Konsep Aplikasi dan Web\\Aplikasi\\flutter_application_2\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Kuliah\\TI\\Semester 5\\Konsep Aplikasi dan Web\\Aplikasi\\flutter_application_2"
+  "FLUTTER_TARGET=D:\\Kuliah\\TI\\Semester 5\\Konsep Aplikasi dan Web\\Aplikasi\\flutter_application_2\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Kuliah\\TI\\Semester 5\\Konsep Aplikasi dan Web\\Aplikasi\\flutter_application_2\\.dart_tool\\package_config.json"
+)
